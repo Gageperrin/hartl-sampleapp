016e81b27586db13d2037750ef4e015755c727de
Nathaniel Perrin <ngperrin@protonmail.com>
2019-12-30 16:38:17 -0600

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                                  dependent:   :destroy
     has_many :following, through: :active_relationships, source: :followed
       has_many :followers, through: :passive_relationships, source: :follower
-      
+
 
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
@@ -79,7 +79,10 @@ class User < ApplicationRecord
 
 
     def feed
-        Micropost.where("user_id = ?", id)
+      following_ids = "SELECT followed_id FROM relationships
+                       WHERE  follower_id = :user_id"
+      Micropost.where("user_id IN (#{following_ids})
+                       OR user_id = :user_id", user_id: id)
     end
 
     def follow(other_user)
